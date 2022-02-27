df86b7f99e388193d808b0887d9fec3a03ae38ef
Amen <aymantauhid2@gmail.com>
2022-02-28 00:09:56 +0530

    Implemented random dice number function by clicking the button.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton = findViewById<Button>(R.id.rollButton)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+
     }
+    private fun rollDice() {
+        // Toast.makeText(this, "button clicked",
+        //  Toast.LENGTH_SHORT).show()
+        val randomInt = (1..6).random()
+        val messageText = findViewById<TextView>(R.id.messageText)
+        messageText.text = randomInt.toString()
+    }
+
 }
